97a061e5603572fb51ed345ac19b87f74dc5949b
Musashi-miyamoto84 <denya19736@gmail.com>
2026-07-16 00:39:14 +0300

    chore: update Canva template links

--- src/lib/templates.ts
+++ src/lib/templates.ts
@@ -37,29 +37,25 @@ export const tableTemplates: TableTemplate[] = [
     id: 'design-1',
     title: 'Дизайн 1',
     image: design1,
-    canvaUrl:
-      'https://www.canva.com/design/DAHPc_Knw50/f8f9FvRgG1e6pBEcZt6jTg/edit',
+    canvaUrl: 'https://canva.link/pcwhiwjh0czckb3',
   },
   {
     id: 'design-2',
     title: 'Дизайн 2',
     image: design2,
-    canvaUrl:
-      'https://www.canva.com/design/DAHPdJw8zXI/h0eeV-ifOOzthCzWg1tCZg/edit',
+    canvaUrl: 'https://canva.link/allh77b3h7b3o5e',
   },
   {
     id: 'design-3',
     title: 'Дизайн 3',
     image: design3,
-    canvaUrl:
-      'https://www.canva.com/design/DAHPdRRMlIs/K0A1QuojWAMJg8irIPgMAw/edit',
+    canvaUrl: 'https://canva.link/1dv0gx5p8opfooe',
   },
   {
     id: 'design-4',
     title: 'Дизайн 4',
     image: design4,
-    canvaUrl:
-      'https://www.canva.com/design/DAHPdV2LYlA/7rd6xT0R6jvZNS3ubUq8vw/edit',
+    canvaUrl: 'https://canva.link/s1ttid7p0x0vdds',
   },
 ]
 
@@ -69,47 +65,41 @@ export const miniCardTemplates: MiniCardTemplate[] = [
     title: 'Дизайн 1',
     front: minicard1F,
     back: minicard1B,
-    canvaUrl:
-      'https://www.canva.com/design/DAHPdYEoEOY/K6-pGmUntzV7Jp6mJZYo9g/edit',
+    canvaUrl: 'https://canva.link/zy35mxb7xr6pctq',
   },
   {
     id: 'minicard-2',
     title: 'Дизайн 2',
     front: minicard2F,
     back: minicard2B,
-    canvaUrl:
-      'https://www.canva.com/design/DAHPdWbsL8w/HIWCmM161AntUao2PH7SDw/edit',
+    canvaUrl: 'https://canva.link/t5lfq1fzbuw40v3',
   },
   {
     id: 'minicard-3',
     title: 'Дизайн 3',
     front: minicard3F,
     back: minicard3B,
-    canvaUrl:
-      'https://www.canva.com/design/DAHPdjmlJ6k/geVGNCBAjiF9NKl368vjaA/edit',
+    canvaUrl: 'https://canva.link/oeokwnuxjkj0lbh',
   },
   {
     id: 'minicard-4',
     title: 'Дизайн 4',
     front: minicard4F,
     back: minicard4B,
-    canvaUrl:
-      'https://www.canva.com/design/DAHPd65fzlc/PSFA65yYnHQ-AQKQ9a97Jw/edit',
+    canvaUrl: 'https://canva.link/9osfup7l68j17jd',
   },
   {
     id: 'minicard-5',
     title: 'Дизайн 5',
     front: minicard5F,
     back: minicard5B,
-    canvaUrl:
-      'https://www.canva.com/design/DAHPdzkk-yY/K9Yk2kCLOyPCQE3-XTlinA/edit',
+    canvaUrl: 'https://canva.link/rjfgzsxpuhwirtm',
   },
   {
     id: 'minicard-6',
     title: 'Дизайн 6',
     front: minicard6F,
     back: minicard6B,
-    canvaUrl:
-      'https://www.canva.com/design/DAHPd35PMoI/t1jRA5QXLqkwhP29jRyfUg/edit',
+    canvaUrl: 'https://canva.link/tll5b3xajfrfxnk',
   },
 ]
